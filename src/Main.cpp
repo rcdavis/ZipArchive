89b533d3079c300bfcf2341dae45b6438d2c3fe8
Ren Davis <rcdavisgd@gmail.com>
2025-06-21 05:37:02 -0400

    Added project structure (#1)
    * Added starting archive files
    
    * Added editorconfig and vs code files
    
    * Added copying of assets for builds
    
    * Removed no longer used define

---
+++ src/Main.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <zip.h>
+
+#include "ZipArchive.h"
+
+struct ZipSourceContents {
+	std::string filename;
+	std::string contents;
+};
+
+int main() {
+	std::vector<ZipSourceContents> sources = {
+		{ "Hello.txt", "Hello from libzip" },
+		{ "Bye.txt", "Bye from libzip" },
+		{ "Dir/First.txt", "Text within Dir" }
+	};
+
+	ZipArchive archive;
+	archive.Open("Output.zip", true);
+	for (const auto &source : sources) {
+		archive.AddText(source.filename, source.contents);
+	}
+
+	// TODO: Why does GetText not work without closing and reopening the zip archive?
+	// Can I not write and then read from the zip archive?
+	// Or is it the flag I pass to Open?
+	archive.Open("Output.zip");
+
+	auto entries = archive.GetEntries();
+	for (const auto &entry : entries) {
+		auto contents = archive.GetText(entry);
+		std::cout << "File" << std::endl;
+		std::cout << "  Name: " << entry << std::endl;
+		std::cout << "  Contents: " << contents << std::endl;
+	}
+
+	archive.Open("Output2.zip", true);
+	for (const auto &source : sources) {
+		archive.AddText(source.filename, source.contents);
+	}
+
+	archive.AddFile("assets/textures/MenuPointer.png");
+
+	entries = archive.GetEntries();
+	for (const auto &entry : entries) {
+		std::cout << "File" << std::endl;
+		std::cout << "  Name: " << entry << std::endl;
+	}
+
+	return 0;
+}
